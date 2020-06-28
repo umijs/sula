e47d0846ddb94c42777bdd7e3485c23268f148d3
xz8la8 <174785876@qq.com>
2020-06-28 10:42:16 +0800

    test: remove time zone

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-process.env.TZ = 'UTC';
-
 module.exports = {
   testPathIgnorePatterns: ['/packages/dumi/lib/', '/packages/preset-dumi/lib/'],
   snapshotSerializers: [require.resolve('enzyme-to-json/serializer')],
